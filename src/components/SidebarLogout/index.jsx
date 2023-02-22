515a04705b673e624bd5cc09ba8702cc2d721fbc
Ramon Barbosa <141barbosa@gmail.com>
2023-02-22 14:14:09 +0000

    Update 4 files

--- src/components/SidebarLogout/index.jsx
+++ src/components/SidebarLogout/index.jsx
@@ -1,78 +1,92 @@
-import { Grid } from "@mui/material";
-import ufopaLogoImg from '../../assets/ufopa-logo.png';
-import logoImg from '../../assets/LOGO.svg';
+import { Grid, Hidden, Paper } from "@mui/material";
+import ufopaLogoImg from "../../assets/ufopa-logo.png";
+import logoImg from "../../assets/LOGO.svg";
+import { Box } from "@mui/system";
 
 const classes = {
-    root: {
-        height: 57,
-        backgroundColor: '#02121D',
-        padding: '0px 83px',
+  root: {
+    height: 57,
+    backgroundColor: "#02121D",
+    padding: "0px 7%",
+  },
+  rootGrid: {
+    color: "white",
+  },
+  left: {
+    fontFamily: "Comfortaa",
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "16px",
+    lineHeight: "36px",
+    color: "#FFFFFF",
+  },
+  lblUniversity: {
+    width: "100%",
+    flexWrap: "none",
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontSize: 12,
+    display: {
+      xs: "none",
+      sm: "block",
+      md: "block",
+      lg: "block",
     },
-    rootGrid: {
-        color: 'white'
-    },
-    left: {
-        fontFamily: 'Comfortaa',
-        fontStyle: 'normal',
-        fontWeight: 700,
-        fontSize: '16px',
-        lineHeight: '36px',
-        color: '#FFFFFF',
-    },
-    right: {
-        textAlign: 'center',
-        fontFamily: 'Roboto',
-        fontSize: 12
-
-    }
-}
-
+  },
+};
 
 export default function SidebarLogout() {
+  return (
+    <div style={classes.root}>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        style={classes.rootGrid}
+      >
+        <Grid
+          container
+          item
+          xs={6}
+          sm={3}
+          md={3}
+          lg={3}
+          alignItems="center"
+          style={classes.left}
+        >
+          <Grid item style={{ marginBottom: -10 }}>
+            <img src={logoImg} width={30} />
+          </Grid>
 
-    return (
-        <div style={classes.root}>
-            <Grid
-                container
-                justifyContent='space-between'
-                alignItems='center'
-                style={classes.rootGrid}
-            >
-                <Grid
-                    container
-                    item
-                    sm={3} md={3} lg={3}
-                    alignItems='center'
-                    style={classes.left}
-                >
-                    <Grid item style={{ marginBottom: -10 }}>
-                        <img src={logoImg} width={30} />
-                    </Grid>
+          <Grid item style={{ fontFamily: "Comfortaa" }}>
+            prodisciplina
+          </Grid>
+        </Grid>
 
-                    <Grid item style={{ fontFamily: 'Comfortaa' }}>
-                        prodisciplina
-                    </Grid>
-                </Grid>
+        <Grid container item xs={4} sm={4} md={4} lg={4} alignItems="center">
+          <Grid
+            item
+            xs={12}
+            sm={5}
+            md={5}
+            lg={5}
+            style={{ textAlign: "center" }}
+          >
+            <Box
+              component="img"
+              alt="UFOPA Logo"
+              sx={{ height: { xs: 50, sm: 60, md: 60 } }}
+              src={ufopaLogoImg}
+            />
+          </Grid>
 
-                <Grid
-                    container
-                    item
-                    sm={4} md={4} lg={4}
-                    style={classes.right}
-                    alignItems='center'
-                >
-                    <Grid item sm={6} md={6} lg={6}>
-                        <img width={60} src={ufopaLogoImg} />
-                    </Grid>
-
-                    <Grid item sm={6} md={6} lg={6}>
-                        Universidade Federal do Oeste do Pará
-                        <hr />
-                        UFOPA
-                    </Grid>
-                </Grid>
-            </Grid>
-        </div>
-    );
-
+          <Grid item sm={5} md={5} lg={5} sx={classes.lblUniversity}>
+            Universidade Federal do Oeste do Pará
+            <hr />
+            UFOPA
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
